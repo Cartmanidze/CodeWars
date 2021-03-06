319783da46a7c86771b2ac00cd2ff6d4c80f2a95
Глеб Безруков <bezrukov96@outlook.com>
2021-03-06 12:19:03 +0300

    kata

---
+++ CodeWars/Kata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace CodeWars
+{
+    public class Kata
+    {
+        public static string PigIt(string str)
+        {
+            if (string.IsNullOrEmpty(str)) throw new ArgumentException($"{nameof(str)} is null or empty");
+            var words = str.Split(' ');
+            StringBuilder result = new StringBuilder();
+            foreach (var word in words)
+            {
+                if (char.IsPunctuation(word, 0))
+                {
+                    result.Append(word + ' ');
+                }
+                else
+                {
+                    var lastChar = word[0];
+                    var newWord = word.Remove(0, 1) + lastChar + "ay ";
+                    result.Append(newWord);
+                }
+            }
+            return result.ToString(0, result.Length - 1);
+        }
+    }
+}
---
+++ CodeWars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Kata.PigIt("Pig latin is cool");
+        }
+    }
+}
